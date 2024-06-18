9878b6120d68f438790e0ea32ec3eb59b731a7f4
Jake Motta <jake@transcrypts.com>
2024-06-18 12:42:54 -0700

    metadata example

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: 'About', // Overrides the title from `app/layout.tsx`
+  // `description` is automatically pulled from `app/layout.tsx`
+};
+
 export default function About() {
   return <div>About</div>;
 }
