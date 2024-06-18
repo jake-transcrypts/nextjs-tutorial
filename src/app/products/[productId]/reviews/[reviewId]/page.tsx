905277e378d78a7bbdffb38a58935a06971dca24
Jake Motta <jake@transcrypts.com>
2024-06-18 11:58:29 -0700

    Send user to not-found page

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,4 +1,11 @@
+import { notFound } from 'next/navigation';
+
 export default function ProductReviewDetails({ params }: { params: { productId: string; reviewId: string } }) {
+  // Return 'not-found' page if reviewId is greater than 100
+  if (parseInt(params.reviewId) > 100) {
+    notFound();
+  }
+
   return (
     <div>
       Review Details for {params.productId} / {params.reviewId}
