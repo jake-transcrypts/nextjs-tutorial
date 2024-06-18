f37289d90c4b2767e287d7c0a117ae9c79717034
Jake Motta <jake@transcrypts.com>
2024-06-18 12:09:25 -0700

    Private folders and underscore urls

---
+++ src/app/%5Flibrary/page.tsx
@@ -0,0 +1,4 @@
+// URL will be `localhost:3000/_library`
+export default function RouteWithAnUnderscore() {
+  return <div>You can view this in the browser</div>;
+}
---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,4 @@
+// This file is unroutable at `localhost:3000/_lib`
+export default function PrivateRoute() {
+  return <div>You cannot view this in the browser</div>;
+}
