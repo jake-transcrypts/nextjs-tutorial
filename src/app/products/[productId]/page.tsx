2a5e808557172e238d4a4e1bd190f813de73b15c
Jake Motta <jake@transcrypts.com>
2024-06-18 12:51:16 -0700

    generateMetadata example

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,3 +1,26 @@
-export default function ProductDetails({ params }: { params: { productId: string } }) {
+import { Metadata } from 'next';
+
+type Props = {
+  params: {
+    productId: string;
+  };
+};
+
+// This function is used to dynamically generate metadata for the page
+export const generateMetadata = async ({ params }: Props): Promise<Metadata> => {
+  // Can make a async call to a database or an API to get the metadata
+  const description = await new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve(`Description for product ${params.productId}`);
+    }, 3000);
+  });
+
+  return {
+    title: `Product ${params.productId}`,
+    description,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
   return <div>Details about product {params.productId}</div>;
 }
