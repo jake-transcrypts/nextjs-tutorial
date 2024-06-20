8dcda30a7447b4e0cd0f2cd974ef71b92b5a8f29
Jake Motta <jake@transcrypts.com>
2024-06-20 16:17:25 -0600

    Template

--- src/app/(auth)/template.tsx
+++ src/app/(auth)/template.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useState } from 'react';
 import './styles.css';
 
 const navLinks = [
@@ -9,11 +10,19 @@ const navLinks = [
   { name: 'Forgot Password', href: '/forgot-password' },
 ];
 
+/**
+ * Template.tsx will cause 'input' to reset on page change, as opposed to layout.tsx which will not.
+ */
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
+  const [input, setInput] = useState('');
 
   return (
     <div>
+      <div>
+        <input type="text" placeholder="placeholder" value={input} onChange={(e) => setInput(e.target.value)} />
+      </div>
       {navLinks.map((link) => {
         const isActive = pathname.startsWith(link.href);
 
